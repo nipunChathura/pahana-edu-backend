9ff8259a71857964eb6cff30600d3ec84d3fba7c
nipun.chathuranga <nipun.chathuranga@ideahub.lk>
2025-08-01 00:04:27 +0530

    Fixing category bugs

--- src/main/java/com/icbt/pahanaedu/service/CategoryService.java
+++ src/main/java/com/icbt/pahanaedu/service/CategoryService.java
@@ -5,6 +5,7 @@ import com.icbt.pahanaedu.dto.CategoryManageDto;
 public interface CategoryService {
     public CategoryManageDto addCategory(CategoryManageDto categoryManageDto);
     public CategoryManageDto updateCategory(CategoryManageDto categoryManageDto);
+    public CategoryManageDto deleteCategory(CategoryManageDto categoryManageDto);
     public CategoryManageDto getCategoryById(CategoryManageDto categoryManageDto);
     public CategoryManageDto getCategories(CategoryManageDto categoryManageDto);
 }
